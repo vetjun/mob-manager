718a603ba15831a1406278cc267ddef37a3f61da
osman.cevik <osman@iqvizyon.com>
2022-02-25 20:21:08 +0300

    some models

---
+++ database/migrations/2022_02_25_164432_create_bids_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBidsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bids', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_name');
+            $table->string('bid_code');
+            $table->string('bid_amount');
+            $table->dateTime('bid_date');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bids');
+    }
+}
